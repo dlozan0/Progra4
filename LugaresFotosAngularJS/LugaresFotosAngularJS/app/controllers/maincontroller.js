b4b2a332b6ccf4b5c3941ca0ff075e8831a4b78a
dlozan0 <dlozano.cas@gmail.com>
2017-02-10 23:52:50 -0600

    avance 10-02-17 terminado

--- LugaresFotosAngularJS/LugaresFotosAngularJS/app/controllers/maincontroller.js
+++ LugaresFotosAngularJS/LugaresFotosAngularJS/app/controllers/maincontroller.js
@@ -1,9 +1,28 @@
-﻿app.controller('bookStore', function ($scope) {
+﻿app.controller('BookStore', function ($scope) {
     $scope.items =
         [
-            { ISBN: "5674789", Nombre: "ASP.NET MVC", precio: 560, cantidad: 20 },
-            { ISBN: "4352134", Nombre: "AngularJS", precio: 450, cantidad: 25 },
-            { ISBN: "2460932", Nombre: "JavaScript", precio: 180, cantidad: 15 },
+            { ISBN: "5674789", Nombre: "ASP.NET MVC", Precio: 560, Cantidad: 20 },
+            { ISBN: "4352134", Nombre: "AngularJS", Precio: 450, Cantidad: 25 },
+            { ISBN: "2460932", Nombre: "JavaScript", Precio: 180, Cantidad: 15 },
 
         ];
+    $scope.editing = false;
+    $scope.totalPrice = function () {
+        var total = 0;
+        for (i = 0; i < $scope.items.length; i++) {
+            total += $scope.items[i].Precio * $scope.items[i].Cantidad;
+        }
+        return total;
+    };
+    $scope.removeItem = function(index) {
+        $scope.items.splice(index, 1);
+    }
+    $scope.editItem = function(index) {
+        $scope.editing = $scope.items.indexOf(index);
+    }
+    $scope.saveField = function(index) {
+        if ($scope.editing !== false) {
+            $scope.editing = false;
+        }
+    };
 });
